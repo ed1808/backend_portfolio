6ea503d75219dee79c5f49d417576d473b300019
Edward Rodríguez <erodriguez@comunike.com>
2024-10-29 12:57:13 -0500

    Remove staticfiles dirs

--- portfolio_api/settings.py
+++ portfolio_api/settings.py
@@ -115,8 +115,6 @@ STATIC_ROOT = "static"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = "media"
 
-STATICFILES_DIRS = [BASE_DIR / "static"]
-
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
