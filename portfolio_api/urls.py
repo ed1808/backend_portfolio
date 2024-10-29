16c8bae5a3ca0cb63b713cba049bc03492765c13
Edward Rodríguez <erodriguez@comunike.com>
2024-10-28 23:09:35 -0500

    Update static config

--- portfolio_api/urls.py
+++ portfolio_api/urls.py
@@ -3,15 +3,8 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
-if settings.DEBUG:
-    urlpatterns = [
-        path("admin/", admin.site.urls),
-        path("api/", include("projects.urls")),
-        path("api/", include("contacts.urls")),
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    urlpatterns = [
-        path("admin/", admin.site.urls),
-        path("api/", include("projects.urls")),
-        path("api/", include("contacts.urls")),
-    ]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/", include("projects.urls")),
+    path("api/", include("contacts.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
